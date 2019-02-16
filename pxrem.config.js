35cb4824ac1590955bb8073f8a8f4b3aa3437058
Leonardo Quevedo <pac.leo@hotmail.com>
2019-02-15 23:22:23 -0200

    refact(bundle): Improves the way the module is bundled.

---
+++ pxrem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  patterns: "./src/**/*.scss",
+  ignoreCss: ["min-width", "max-width", "min-height", "max-height"],
+  pxToRemRatio: 0.1,
+  isReplace: true,
+  convertBorder1px: false
+};
